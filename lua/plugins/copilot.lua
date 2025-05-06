455226f86fc7e67458128f8866f4d4d6f0025f36
noahfrn <fourniernoah6@gmail.com>
2025-05-06 12:09:46 +0100

    init

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		opts = {
+			suggestion = { enabled = true },
+			panel = { enabled = false },
+		},
+	},
+}
